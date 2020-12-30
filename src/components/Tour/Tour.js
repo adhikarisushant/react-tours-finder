475669f409550ad9a93522fb41ff0798ce20ecb1
Sushant <sushant.adhikari.619@gmail.com>
2020-12-30 14:31:55 +0545

    fetching information from the procided data

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,17 +3,18 @@ import './Tour.scss';
 
 export default class Tour extends Component {
     render() {
+        const { city, img, name, info } = this.props.tour;
         return (
             <article className="tour">
                 <div className="img-container">
-                <img src="https://images.pexels.com/photos/1531660/pexels-photo-1531660.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" />
+                <img src={img} alt="" />
                 <span className="close-btn"><i className="fas fa-window-close" /></span>
                 </div>
                 <div className="tour-info">
-                    <h3>city</h3>
-                    <h4>name</h4>
-                    <h5>info <span><i className="fas fa-caret-square-down" /></span></h5>
-                    <p>lorem is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</p>
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5>info{" "} <span><i className="fas fa-caret-square-down" /></span></h5>
+                    <p>{info}</p>
                 </div>
             </article>
         );
